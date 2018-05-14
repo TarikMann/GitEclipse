3550bfc2afaadef47ac0595bfc8affb2bffd1f54
Tarik Mannou <m.tarik.contact@gmail.com>
2018-05-14 15:48:56 +0200

    C5

--- GitEclipse/src/fr/gtm/Lanceur.java
+++ GitEclipse/src/fr/gtm/Lanceur.java
@@ -5,6 +5,8 @@ public class Lanceur {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helloo");
+		System.out.println("Partie Service");
+		System.out.println("C3");
 		System.out.println("On avance a la deuxieme partie");
 	}
 
